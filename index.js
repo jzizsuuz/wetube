08c1bccf4a9f6ebff0f1d762c5e9f820b9b5cade
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2020-06-18 01:58:46 +0900

    middleWare practice

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet"; // for security
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -7,13 +11,16 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
 
-function handleHome(req, res) {
-  res.send("Hello from home");
-}
+const handleHome = (req, res) => res.send("Hello from my ass");
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+// middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
