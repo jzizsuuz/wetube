21de57de722335895f1f7b89574951b758b157e8
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2020-06-19 22:09:54 +0900

    MVC pattern commit

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet"; // for security
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
-
-const PORT = 4000;
-
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 // middleware
 app.use(cookieParser());
@@ -20,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
