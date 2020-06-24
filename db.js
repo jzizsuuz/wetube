b1ff6223f1a438f23ff4d2c4222b571984b1dddc
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2020-06-24 23:46:09 +0900

    #2.22 Home Controller

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ export const videos = [
     title: "Video awesome",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creators: {
       id: 121212,
       name: "JisuLee",
@@ -16,7 +17,8 @@ export const videos = [
     title: "Video super",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creators: {
       id: 121212,
       name: "JisuLee",
@@ -28,7 +30,8 @@ export const videos = [
     title: "Video nice",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creators: {
       id: 121212,
       name: "JisuLee",
@@ -40,7 +43,8 @@ export const videos = [
     title: "Video perfect",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creators: {
       id: 121212,
       name: "JisuLee",
