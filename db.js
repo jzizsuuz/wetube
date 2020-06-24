c93f1e1bf447dcef27a6ea16b91bda088306b94b
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2020-06-24 23:30:59 +0900

    #2.20-2.21 Change Profile HTML and Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 121212,
+      name: "JisuLee",
+      email: "jisu@lee.com",
+    },
+  },
+  {
+    id: 111111,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 121212,
+      name: "JisuLee",
+      email: "jisu@lee.com",
+    },
+  },
+  {
+    id: 22222,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 121212,
+      name: "JisuLee",
+      email: "jisu@lee.com",
+    },
+  },
+  {
+    id: 55555,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 121212,
+      name: "JisuLee",
+      email: "jisu@lee.com",
+    },
+  },
+];
