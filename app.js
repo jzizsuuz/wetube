65a66c935e2a4cf2cc4eaa05d6eaec0f2526c6ec
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2020-06-26 01:09:52 +0900

    #3.0-3.6 Uploading and Creating a Video 1

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
-
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
