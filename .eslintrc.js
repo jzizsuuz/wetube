46fd45187e6db4fd58846a0a08d26c6e2516dc36
Jisu Lee <51789449+jzizsuuz@users.noreply.github.com>
2020-07-05 05:10:32 +0900

    #4.1-4.2 Styles with Webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
-  // env: {
-  //   browser: true,
-  //   es2020: true,
-  //   node: true,
-  // },
-  extends: [["airbnb-base", "some-other-config-you-use", "prettier"]],
-  // parserOptions: {
-  //   ecmaVersion: 11,
-  //   sourceType: "module",
-  // },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
   rules: {},
 };
